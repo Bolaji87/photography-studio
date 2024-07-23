c60527556509f97803df9499a54a14c84feada11
Bolaji87 <sakabolaji77@gmail.com>
2024-07-23 21:10:07 +0100

    set up react project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HomePage from "./pages/Home/HomePage.jsx";
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function NavBar() {
+  return (
+    <header>
+      <h1>slacrane shot it</h1>
+      <nav></nav>
+      <div className="log">h</div>
+    </header>
+  );
+}
+
+export default NavBar;

62de56e85a7c6011d5d091c23d6e12e5b762bb34
Bolaji87 <sakabolaji77@gmail.com>
2024-07-26 13:11:14 +0100

    add hero component

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,21 +1,64 @@
 import React from "react";
-import coverImg from "../../assets/slacrane/slacrane-1.jpg";
 
 import "./hero.css";
+const items = [
+  {
+    name: " work",
+    image: "./slacrane/studio-1.jpg",
+    id: crypto.randomUUID(),
+  },
+  {
+    name: " work",
+    image: "./slacrane/studio-2.jpg",
+    id: crypto.randomUUID(),
+  },
+  {
+    name: " work",
+    image: "./slacrane/studio-3.jpg",
+    id: crypto.randomUUID(),
+  },
+
+  {
+    name: " work",
+    image: "./slacrane/studio-4.jpg",
+    id: crypto.randomUUID(),
+  },
+  {
+    name: " work",
+    image: "./slacrane/slacrane-1.jpg",
+    id: crypto.randomUUID(),
+  },
+  {
+    name: " work",
+    image: "./slacrane/slacrane-3.jpg",
+    id: crypto.randomUUID(),
+  },
+];
 
 function Hero() {
   return (
     <div id="hero">
-      <div className="hero-display-text">
-        <h1>Slacrane shot it</h1>
-        <button>
-          <a href="#">
-            Book a Session <span>&rarr;</span>
-          </a>
-        </button>
+      <h1>Catalogue</h1>
+      <div className="hero-images">
+        <ul>
+          {items.map((item) => (
+            <Item key={item.id} item={item} />
+          ))}
+        </ul>
       </div>
     </div>
   );
 }
 
+function Item({ item }) {
+  return (
+    <li className="list-images">
+      <img src={item.image} alt={item.name} />
+      {/* <h4> Catalogue {item.name}</h4> */}
+      <button>
+        <a href="#">Book a session</a>
+      </button>
+    </li>
+  );
+}
 export default Hero;

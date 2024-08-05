e94825035c5263e3b0283cde511663d8a43f3021
bolaji87 <sakabolaji77@gmai.com>
2024-08-05 11:29:45 -0700

    enable page not found to go back to home

--- src/pages/pagenotfound/PageNotFound.jsx
+++ src/pages/pagenotfound/PageNotFound.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import "./pagenotfound.css";
+import { Link } from "react-router-dom";
 
 function PageNotFound() {
   return (
     <div className="notfound">
       <h1>Page not found</h1>
+
+      <p>
+        <Link to="/">Go back to home</Link>
+      </p>
     </div>
   );
 }
